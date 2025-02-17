5c2acdf274ec0e1fc7854cb0efb6d8d5b565ddbc
Ethan Steinfeld <ethansteinfeld1@gmail.com>
2025-02-17 18:39:42 -0500

    Added Details to Platform

--- src/level/platform.rs
+++ src/level/platform.rs
@@ -4,7 +4,36 @@ use bevy_rapier2d::prelude::*;
 
 use crate::shared::GroupLabel;
 
-/// Component for moving platforms
-#[derive(Default, Component)]
-pub struct MovingPlatform;
+struct Position {
+    x: i32,
+    y: i32
+}
 
+/// Component for platforms
+#[derive(Default, Component)]
+pub struct Platform {
+
+}
+
+/// Bundle for moving platforms
+#[derive(Default, Bundle)]
+pub struct MovingPlatformBundle {
+    platform: Platform
+}
+
+fn spawn_platforms(query: Query<>) {
+
+}
+
+
+/*
+
+Platforms have:
+    * Position -> (x,y)
+    * Path -> Vec<>
+    * Speed -> (x)
+    * Triggered/Continuous -> enum(Triggered/Continous)
+    * Repeat -> boolean
+    * DefaultActive
+    * IsActive
+*/
