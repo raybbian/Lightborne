5f251f05d70641458232c553bb45edc9b9bad5ce
Raymond Bian <raybbian@gmail.com>
2025-04-11 14:58:27 -0400

    added treebranch

--- src/level/decoration.rs
+++ src/level/decoration.rs
@@ -3,6 +3,16 @@ use bevy_ecs_ldtk::prelude::*;
 
 use crate::lighting::LineLight2d;
 
+pub struct DecorationPlugin;
+
+impl Plugin for DecorationPlugin {
+    fn build(&self, app: &mut App) {
+        app.register_ldtk_entity::<LdtkTreeBranchBundle>("Treebranch")
+            .register_ldtk_entity::<LdtkLanternBundle>("Lantern")
+            .register_ldtk_entity::<LdtkLantern2Bundle>("Lantern2");
+    }
+}
+
 #[derive(Component, Default)]
 pub struct Lantern;
 
@@ -28,4 +38,15 @@ pub struct LdtkLantern2Bundle {
 
 pub fn lantern_light(_: &EntityInstance) -> LineLight2d {
     LineLight2d::point(Vec4::new(1.0, 0.8627, 0.7176, 1.0), 50.0, 0.02)
+}
+
+#[derive(Component, Default)]
+pub struct TreeBranch;
+
+#[derive(Bundle, LdtkEntity)]
+pub struct LdtkTreeBranchBundle {
+    #[sprite_sheet]
+    sprite: Sprite,
+    #[default]
+    lantern: TreeBranch,
 }
